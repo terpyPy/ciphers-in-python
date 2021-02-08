a92cb8603b3ecfd981c9cb5ce08ba5d600f9a9cd
terpyPy <66324329+terpyPy@users.noreply.github.com>
2021-02-08 17:39:53 -0500

    new event loop
    changed how the program loops, and checks per loops changed.

--- passwordGen/passtool.py
+++ passwordGen/passtool.py
@@ -6,28 +6,28 @@
 import allTheFuctions
 import random
 import sys
+mode = ''
+while True:
+    key = random.randrange(1, 94)
+    mode = (input('encrypt/decrypt?: ', ))
+    scram = ''
+    if mode == "encrypt":
+        password = (input('Enter a phrase you want to use: ', ))
+        scram = (allTheFuctions.encryptMessage(key, password))
+        scram = (allTheFuctions.ceaser(key, scram, mode))
+        print('hash: ', scram, '\n', 'key: ', key)
 
-key = random.randrange(1, 94)
-mode = (input('encrypt/decrypt?: ', ))
-scram = ''
-if mode == 'e' or "encrypt":
-    password = (input('Enter a phrase you want to use: ', ))
-
-    scram = (allTheFuctions.encryptMessage(key, password))
-    scram = (allTheFuctions.ceaser(key, scram, mode))
-    print('hash:', scram)
-
-
-mode = input('decrypt or quit?:', )
-# i need to create a thingUndo function decryption work with the reversing
-# and take multi key input to undo the 2 current layers i.e. key1 and key2
-# d1B17po7T1Id4E1
-if mode == 'decrypt':
-
-    encryptPass = scram
-    unScram = allTheFuctions.ceaserUndo(key, encryptPass, mode)
-    unScram = allTheFuctions.decryptMessage(key, unScram)
-    print('removed sudo hash:', unScram)
-if mode == 'q' or 'quit':
-    print('closing tool')
-    sys.exit()
+    # i need to create a thingUndo function decryption work with the reversing
+    # and take multi key input to undo the 2 current layers i.e. key1 and key2
+    # d1B17po7T1Id4E1
+    elif mode == 'decrypt':
+        key = int(input('key used?: '))
+        encryptPass = input('phrase used?: ')
+        unScram = allTheFuctions.ceaserUndo(key, encryptPass, mode)
+        unScram = allTheFuctions.decryptMessage(key, unScram)
+        print('removed sudo hash:', unScram)
+    elif mode == 'q' or 'quit':
+        print('closing tool')
+        sys.exit()
+    else:
+        continue
